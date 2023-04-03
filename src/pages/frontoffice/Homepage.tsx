072422a5486dc43d56a72b474e19390fb083a268
ace34TT <tafinasoa35@gmail.com>
2023-04-03 13:40:05 +0300

    style : homepage

--- src/pages/frontoffice/Homepage.tsx
+++ src/pages/frontoffice/Homepage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Homepage() {
   return (
-    <div className="-mt-20 h-screen flex flex-col justify-center items-center min-h-[940px]">
+    <div className=" h-screen flex flex-col justify-center items-center min-h-[940px]">
       {/* <img src={img} alt="" /> */}
       <div className="text-3xl text-center">
         Entre tradition et modernité symbole fort de liberté <br /> et
