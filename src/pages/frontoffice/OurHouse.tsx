a04660540639bbeb9fcb8ec8619fca9ac210d102
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2023-04-03 12:21:44 +0300

    feat : translation of our house and studio

--- src/pages/frontoffice/OurHouse.tsx
+++ src/pages/frontoffice/OurHouse.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import img from "./../../assets/3d.png";
+import { useTranslation } from "react-i18next";
 export default function OurHouse() {
+  const { t } = useTranslation();
   return (
     // <div className="-mt-20 h-screen flex flex-col justify-center items-center min-h-[940px]">
     //   <img src={img} alt="" />
@@ -12,47 +14,18 @@ export default function OurHouse() {
     <div className="flex justify-center py-16 prose max-w-none text-2xl w-screen">
       <div className="w-4/12 text-black dark:text-white  ">
         <p>
-          Plongez dans l'univers intemporel des bijoux Trinitée, une véritable
-          ode à l'histoire et à l'artisanat d'antan. <br /> Chacune des pièces
-          de notre collection évoque un trésor ancien comme déniché par un
-          archéologue passionné.
+          {t("OurHouse.trad1")}
+          <br />
+          {t("OurHouse.trad2")}
         </p>
         <br />
-        <p>
-          Créée en 2022 par Antsa Gaëlle, notre marque est profondément inspirée
-          des bijoux traditionnels de Madagascar, reflétant la richesse du pays
-          où l'histoire culturelle, la durabilité et le savoir-faire artisanal
-          sont mis en avant pour vous offrir des créations d'exception.
-        </p>
-        <p>
-          Notre emblématique bracelet Vango Vango est plus qu'un simple bijou,
-          c'est un symbole de l'histoire et de la culture de Madagascar, fruit
-          d'un savoir-faire ancestral transmis de génération en génération. Avec
-          son design unique et son charme intemporel, il est l'accessoire
-          parfait pour les clients les plus exigeants, qui cherchent à se
-          démarquer avec style et élégance.
-        </p>
+        <p>{t("OurHouse.trad3")}</p>
+        <p>{t("OurHouse.trad4")}</p>
         <br />
-        <p>
-          Chez Trintée, chaque bijoux est une pièce unique, créée avec passion
-          et savoir-faire artisanal, pour vous offrir une expérience unique et
-          intemporelle, où la qualité, l'éthique et la durabilité sont au cœur
-          de notre engagement.
-        </p>
-        <p>
-          Nous sommes fiers de mettre en lumière les traditions malgaches tout
-          en apportant notre touche personnelle à chacune de nos créations.
-        </p>
+        <p>{t("OurHouse.trad5")}</p>
         <br />
-        <p>
-          Plongez dans notre univers d'élégance et de raffinement, où le temps
-          semble suspendu et où chaque bijou est chargé d'une symbolique forte
-          et émotionnelle.
-        </p>
-        <p>
-          Rejoignez-nous dans cette aventure hors du temps et découvrez notre
-          collection de bijoux inspirée par l'histoire et la culture malgaches.
-        </p>
+        <p>{t("OurHouse.trad6")}</p>
+        <p>{t("OurHouse.trad7")}</p>
       </div>
     </div>
   );
--- src/pages/frontoffice/Workshop.tsx
+++ src/pages/frontoffice/Workshop.tsx
@@ -1,57 +1,79 @@
 import React from "react";
-
+import { useTranslation } from "react-i18next";
 export default function Workshop() {
+  const { t } = useTranslation();
   return (
+    
     <div className="flex justify-center py-16 prose max-w-none text-2xl w-screen">
       <div className="w-8/12 text-black dark:text-white  ">
         <p>
-          Nous utilisons des matériaux précieux soigneusement sélectionnés pour
+        {t("Studio.trad1")}
+          {/* Nous utilisons des matériaux précieux soigneusement sélectionnés pour
           leur qualité et leur origine éthique, tout en valorisant le travail
           des artisans et en leur offrant des conditions de travail équitables.
           En choisissant notre marque, vous êtes non seulement assurés
           d'acquérir des bijoux de haute qualitée, mais également de contribuer
           à la préservation des traditions artisanales de Madagascar, de
-          soutenir les travailleurs locaux et de faire un choix éthique.
+          soutenir les travailleurs locaux et de faire un choix éthique. */}
         </p>
         <br />
         <div>
-          Nos métaux précieux
+        {t("Studio.trad2")}
+          {/* Nos métaux précieux */}
           <ul className="m-0">
             <li className="m-0">
-              argent 925 : argent de 1er titre composé de 925 millièmes d’argent
-              et de 75 millièmes de cuivre
+            {t("Studio.trad3")}
+              {/* argent 925 : argent de 1er titre composé de 925 millièmes d’argent
+              et de 75 millièmes de cuivre */}
             </li>
             <li className="m-0">
-              or jaune 750/1000 : or 18 carats composé de 750 millièmes d’or pur
+            {t("Studio.trad4")}
+              {/* or jaune 750/1000 : or 18 carats composé de 750 millièmes d’or pur */}
             </li>
             <li className="m-0">
-              or rouge 750/1000 : or 18 carats composé de 750 millièmes d’or pur
+            {t("Studio.trad5")}
+              {/* or rouge 750/1000 : or 18 carats composé de 750 millièmes d’or pur */}
             </li>
-            <li className="m-0"> or blanc 750/1000 : or 18 carats </li>
+            <li className="m-0"> 
+            {t("Studio.trad6")}
+            {/* or blanc 750/1000 : or 18 carats  */}
+            </li>
           </ul>
         </div>
         <br />
         <div>
-          poinçons et gravures <br />
-          chaque bijou Trinité porte les poinçons et gravures suivantes :
+        {t("Studio.trad7")}
+          {/* poinçons et gravures  */}
+
+          <br />
+          {t("Studio.trad8")}
+          {/* chaque bijou Trinité porte les poinçons et gravures suivantes : */}
           <ul className="m-0">
             <li className="m-0">
-              le poinçon de maître garantissant le lieu de fabrication
+            {t("Studio.trad9")}
+              {/* le poinçon de maître garantissant le lieu de fabrication */}
             </li>
             <li className="m-0">
-              le poinçon de l’état certifiant la qualité du métal précieux :
-              tête d’aigle pour l’or et tête de minerve pour l’argent
+            {t("Studio.trad10")}
+              {/* le poinçon de l’état certifiant la qualité du métal précieux :
+              tête d’aigle pour l’or et tête de minerve pour l’argent */}
             </li>
             <li className="m-0">
-              le poinçon Trinité signe la création originale de notre marque
+            {t("Studio.trad11")}
+              {/* le poinçon Trinité signe la création originale de notre marque */}
             </li>
             <li className="m-0">
-              925 ou 750 attestant de la pureté du métal précieux
+            {t("Studio.trad12")}
+              {/* 925 ou 750 attestant de la pureté du métal précieux */}
             </li>
-            <li className="m-0">le logo Trinité</li>
             <li className="m-0">
-              le numéro de lot qui garantit la traçabilité de production de
-              l’objet
+            {t("Studio.trad13")}
+              {/* le logo Trinité */}
+              </li>
+            <li className="m-0">
+            {t("Studio.trad14")}
+              {/* le numéro de lot qui garantit la traçabilité de production de
+              l’objet */}
             </li>
           </ul>
         </div>
