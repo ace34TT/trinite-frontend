59545ad8d48e6dbe4e54a3fea20b04a32e96ffd3
ace34TT <tafinasoa35@gmail.com>
2023-04-03 12:20:49 +0300

    refacto : homepage

--- src/pages/frontoffice/OurHouse.tsx
+++ src/pages/frontoffice/OurHouse.tsx
@@ -2,13 +2,6 @@ import React from "react";
 import img from "./../../assets/3d.png";
 export default function OurHouse() {
   return (
-    // <div className="-mt-20 h-screen flex flex-col justify-center items-center min-h-[940px]">
-    //   <img src={img} alt="" />
-    //   <div className="text-3xl text-center">
-    //     Entre tradition et modernité symbole fort de liberté <br /> et
-    //     d’émancipation
-    //   </div>
-    // </div>
     <div className="flex justify-center py-16 prose max-w-none text-2xl w-screen">
       <div className="w-4/12 text-black dark:text-white  ">
         <p>
