2d48bc5272d4f318c763afcf0e8f9a73fc8f62e8
ace34TT <tafinasoa35@gmail.com>
2023-04-03 18:30:27 +0300

    test : three fiber

--- src/pages/frontoffice/Homepage.tsx
+++ src/pages/frontoffice/Homepage.tsx
@@ -1,9 +1,11 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
+import ComputersCanvas from "./three-models/MainModel";
 
 export default function Homepage() {
   return (
     <div className=" h-screen flex flex-col justify-center items-center min-h-[940px]">
-      {/* <img src={img} alt="" /> */}
+      <ComputersCanvas></ComputersCanvas>
       <div className="text-3xl text-center">
         Entre tradition et modernité symbole fort de liberté <br /> et
         d’émancipation
