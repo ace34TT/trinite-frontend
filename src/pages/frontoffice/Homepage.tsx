d176497f83372a3a7700d68713696c5d1a858f3a
ace34TT <tafinasoa35@gmail.com>
2023-04-05 13:25:34 +0300

    feat : 3d

--- src/pages/frontoffice/Homepage.tsx
+++ src/pages/frontoffice/Homepage.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Helmet } from "react-helmet";
+// import SphereThreeModel from "../../components/three-model/SphereThreeModel";
+import { Environment } from "@react-three/drei";
+import { Canvas } from "react-three-fiber";
 export default function Homepage() {
   return (
     <>
@@ -7,7 +10,12 @@ export default function Homepage() {
         <title>Trinité - Accueil</title>
       </Helmet>
       <div className=" h-screen flex flex-col justify-center items-center min-h-[940px]">
-        {/* <img src={img} alt="" /> */}
+        <Canvas>
+          <Suspense fallback={null}>
+            {/* <SphereThreeModel /> */}
+            <Environment preset="sunset" background />
+          </Suspense>
+        </Canvas>
         <div className="text-3xl text-center">
           Entre tradition et modernité symbole fort de liberté <br /> et
           d’émancipation
