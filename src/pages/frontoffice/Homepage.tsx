5189f1d15cc50926b6625f31ee86e5ec2ff0a6b5
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2023-04-12 18:32:42 +0300

    fix : loading to speed

--- src/pages/frontoffice/Homepage.tsx
+++ src/pages/frontoffice/Homepage.tsx
@@ -17,7 +17,7 @@ export default function Homepage() {
     for (let i = 0; i < 100; i++) {
       setTimeout(() => {
         setPourcentage(i);
-      }, i * 50); 
+      }, i * 400);
     }
   }, []);
 
