735bfcd9bc490826b8457a5ae1db7d6511725b82
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2023-04-17 13:38:36 +0300

    fix (mainheader) : mobile

--- src/components/main-header/MainHeader.tsx
+++ src/components/main-header/MainHeader.tsx
@@ -14,6 +14,10 @@ const languages = [
   { code: "fr", name: "Français" },
   { code: "en", name: "Anglais" },
 ];
+
+// usage
+// to disable scrolling
+
 const MainHeader = () => {
   const dispatch = useDispatch();
   const theme = useSelector((state: RootState) => state.theme);
@@ -25,18 +29,37 @@ const MainHeader = () => {
     setShowMenu(!showMenu);
   };
 
+  const disableScroll = () => {
+    const scrollTop = document.body.scrollTop;
+    const scrollLeft = document.body.scrollLeft;
+    console.log("====================================");
+    console.log(scrollTop, scrollLeft);
+    console.log("====================================");
+    window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    };
+  };
+
+  // enable scrolling
+  const enableScroll = () => {
+    window.onscroll = function () {};
+  };
+
   return (
-    <header className=" bg-white text-black dark:bg-black dark:text-white">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    <header className=" bg-white text-black dark:bg-black dark:text-white pt-6 ">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 ">
+        <div className="relative flex items-center justify-around h-16 pb-5 sm:pb-0 border-b-2  border-white sm:border-b-0">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden ">
             {/* Bouton pour activer/désactiver le menu mobile */}
             <button
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md  focus:outline-none  focus:ring-inset focus:ring-white pb-5"
               aria-controls="mobile-menu"
               aria-expanded="false"
-              onClick={toggleMenu}
+              onClick={() => {
+                toggleMenu();
+                disableScroll();
+              }}
             >
               <span className="sr-only">Open main menu</span>
               {/*
@@ -45,7 +68,7 @@ const MainHeader = () => {
                 sur les appareils plus grands.
               */}
               <svg
-                className={`${showMenu ? "hidden" : "block"} h-6 w-6`}
+                className={`${showMenu ? "hidden" : "block"} h-10 w-10`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -61,7 +84,7 @@ const MainHeader = () => {
               </svg>
               {/* X icône de fermeture utilisée pour fermer le menu mobile */}
               <svg
-                className={`${showMenu ? "block" : "hidden"} h-6 w-6`}
+                className={`${showMenu ? "block" : "hidden"} h-10 w-10`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -77,109 +100,63 @@ const MainHeader = () => {
               </svg>
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            {/* Logo ou nom du site */}
-            <div className="flex-shrink-0 flex items-center  ">
-              <Link to={"/accueil"}>
-                <img
-                  src={theme.currentTheme === "dark" ? logo[0] : logo[1]}
-                  className="h-24"
-                  alt=""
-                />
-              </Link>
-            </div>
-            {/* Liens de navigation */}
-            <header className=" hidden sm:block sm:ml-6  flex justify-around items-center  py-7 bg-white text-black dark:bg-black dark:text-white">
-              <ul className="flex items-center font-normal gap-10 text-xl">
-                <li>
-                  <Link to={"/notre-maison"}>{t("header.trad1")}</Link>
-                </li>
-                <li>
-                  <Link to={"/trinite"}>Trinité</Link>
-                </li>
-                <li>
-                  <Link to={"/atelier"}>{t("header.trad2")}</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                  <button
-                    onClick={() => {
-                      if (activeLanguage === 0) {
-                        i18n.changeLanguage(languages[1].code);
-                        setActiveLanguage(1);
-                        return;
-                      }
-                      i18n.changeLanguage(languages[0].code);
-                      setActiveLanguage(0);
-                    }}
-                  >
-                    {activeLanguage === 0
-                      ? languages[1].name
-                      : languages[0].name}
-                  </button>
-                </li>
-                <li>
-                  <div className="flex items-center justify-center w-full">
-                    <label
-                      htmlFor="toggleB"
-                      className="flex items-center cursor-pointer"
-                    >
-                      <div className="relative">
-                        <input
-                          type="checkbox"
-                          id="toggleB"
-                          className="sr-only"
-                          onClick={() => {
-                            dispatch(toggleTheme());
-                          }}
-                        />
-                        <div className="block bg-white w-14 h-8 border-2 rounded-md border-black"></div>
-                        <div className="dot absolute left-1 top-1 bg-black w-6 h-6 rounded-md transition"></div>
-                      </div>
-                    </label>
-                  </div>
-                </li>
-              </ul>
-            </header>
-            <div className="text-xl uppercase hidden ">
-              Maison <br /> de haute <br /> joaeollerie
-            </div>
-          </div>
         </div>
       </div>
 
       {/* Menu mobile qui apparaît lorsque le bouton est cliqué */}
       <div
-        className={`${showMenu ? "block" : "hidden"} sm:hidden`}
+        className={`${
+          showMenu ? "block" : "hidden"
+        } sm:hidden pt-20 absolute bg-white dark:bg-black w-[100%] h-screen overscroll-none`}
         id="mobile-menu"
       >
         <div className="px-2 pt-2 pb-3 space-y-1">
-          <a
-            href="#"
-            className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Accueil
+          <a href="#" className="  block px-3 py-3   text-base font-medium">
+            <Link to={"/notre-maison"}>{t("header.trad1")}</Link>
           </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Services
+          <a href="#" className="   block px-3 py-3   text-base font-medium">
+            <Link to={"/trinite"}>Trinité</Link>
           </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            À propos
+          <a href="#" className="   block px-3 py-3   text-base font-medium">
+            <Link to={"/atelier"}>{t("header.trad2")}</Link>
           </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Contact
+          <a href="#" className="   block px-3 py-3  text-base font-medium">
+            <Link to="/contact">Contact</Link>
           </a>
+          <div className="px-3 py-3">
+            <button
+              onClick={() => {
+                if (activeLanguage === 0) {
+                  i18n.changeLanguage(languages[1].code);
+                  setActiveLanguage(1);
+                  return;
+                }
+                i18n.changeLanguage(languages[0].code);
+                setActiveLanguage(0);
+              }}
+            >
+              {activeLanguage === 0 ? languages[1].name : languages[0].name}
+            </button>
+          </div>
+          <div className="px-3 py-3">
+            <label
+              htmlFor="toggleB"
+              className="flex items-center cursor-pointer"
+            >
+              <div className="relative">
+                <input
+                  type="checkbox"
+                  id="toggleB"
+                  className="sr-only"
+                  onClick={() => {
+                    dispatch(toggleTheme());
+                  }}
+                />
+                <div className="block bg-white w-14 h-8 border-2 rounded-md border-black"></div>
+                <div className="dot absolute left-1 top-1 bg-black w-6 h-6 rounded-md transition"></div>
+              </div>
+            </label>
+          </div>
         </div>
       </div>
     </header>
