508c56397a99c0fe791a87b0a916bb1fe617e9d6
ace34TT <tafinasoa35@gmail.com>
2023-04-18 10:27:30 +0300

    try

--- src/pages/frontoffice/portal/Portal.tsx
+++ src/pages/frontoffice/portal/Portal.tsx
@@ -3,34 +3,76 @@ import { Link, useNavigate } from "react-router-dom";
 import "./style.css";
 import logo from "./../../../assets/logo/logo-trinité.png";
 import { Helmet } from "react-helmet";
+import { useGLTF } from "@react-three/drei";
+import { useState, useEffect } from "react";
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Circle002: THREE.Mesh;
+  };
+  materials: {
+    ["Scratched Gold"]: THREE.MeshStandardMaterial;
+  };
+};
+
+function useModelLoading(path: string) {
+  const [model, setModel] = useState<GLTFResult | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  // useEffect(() => {
+  //   useGLTF(path).then((result) => {
+  //     setModel(result);
+  //     setIsLoading(false);
+  //   });
+  // }, [path]);
+  return { model, isLoading };
+}
+
 export default function Portal() {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  //
+  const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   useGLTF("models/final/Bracelet-compressed.glb").then((result) => {
+  //     setModel(result);
+  //     setIsLoading(false);
+  //   });
+  // }, []);
+
   return (
     <>
       <Helmet>
         <title>Trinité</title>
       </Helmet>
-      <div className="h-screen w-screen flex flex-col justify-center gap-32 sm:gap-52 items-center dark:bg-black">
-        <div className="sr-only sm:not-sr-only"></div>
-        <div className="text-white flex flex-col-reverse sm:flex-row items-center gap-16 sm:gap-32">
-          <img src={logo} className="h-28 sm:h-32" alt="" />
-          <div className="the-house text-2xl sm:text-4xl ">
-            MAISON <br /> DE HAUTE <br /> JOAILLERIE
+      <div>
+        {isLoading ? (
+          // Show loading screen when isLoading is true
+          <div>Loading...</div>
+        ) : (
+          // Render the Portal content when the model finishes loading
+          <div className="h-screen w-screen flex flex-col justify-center gap-32 sm:gap-52 items-center dark:bg-black">
+            <div className="sr-only sm:not-sr-only"></div>
+            <div className="text-white flex flex-col-reverse sm:flex-row items-center gap-16 sm:gap-32">
+              <img src={logo} className="h-28 sm:h-32" alt="" />
+              <div className="the-house text-2xl sm:text-4xl ">
+                MAISON <br /> DE HAUTE <br /> JOAILLERIE
+              </div>
+            </div>
+            <div className="flex items-center gap-5 text-white font-light text-3xl">
+              <div
+                className="p-4 cursor-pointer hover:scale-110 transform transition duration-300 ease-in-out"
+                onClick={() => navigate("/accueil")}
+              >
+                {t("homepage_1")}
+              </div>
+              <div className="border border-gray-400 h-16"></div>
+              <div className="p-4 cursor-pointer hover:scale-110 transform transition duration-300 ease-in-out">
+                <Link to={"/contact"}>Contact</Link>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center gap-5 text-white font-light text-3xl">
-          <div
-            className="p-4 cursor-pointer hover:scale-110 transform transition duration-300 ease-in-out"
-            onClick={() => navigate("/accueil")}
-          >
-            {t("homepage_1")}
-          </div>
-          <div className="border border-gray-400 h-16"></div>
-          <div className="p-4 cursor-pointer hover:scale-110 transform transition duration-300 ease-in-out">
-            <Link to={"/contact"}>Contact</Link>
-          </div>
-        </div>
+        )}
       </div>
     </>
   );
