ed8fd0fec7f801638e675e16c072a775be2d1ade
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2023-04-19 00:13:41 +0300

    fix (mobile) : size grid

--- src/pages/frontoffice/Trinity.tsx
+++ src/pages/frontoffice/Trinity.tsx
@@ -16,52 +16,52 @@ export default function Trinity() {
       </Helmet>
       <div className="pt-0 sm:pt-16">
         <div className="grid grid-cols-1 sm:grid-cols-3">
-          <div className="py-16 px-14 flex items-center justify-center border border-gray-300">
+          <div className="py-24 px-14 flex items-center justify-center border border-gray-300">
             {t("Trinite.trad1")}
           </div>
-          <div className=" py-16 px-14 flex flex-col items-center justify-start border border-gray-300">
+          <div className="pt-20 pb-11 px-14 flex flex-col items-center justify-start border border-gray-300">
             <div className="flex justify-between gap-10">
               <p>{t("Trinite.trad2")}</p>
               <p>2300€</p>
             </div>
             <img src={images[0]} alt="" />
           </div>
-          <div className=" py-16 px-14 flex flex-col items-center justify-start border border-gray-300">
+          <div className="pt-24 pb-20 px-14 flex flex-col items-center justify-start border border-gray-300">
             <div className="flex justify-between gap-10">
               <p>{t("Trinite.trad3")}</p>
               <p>660€</p>
             </div>
             <img className="justify-self-center" src={images[1]} alt="" />
           </div>
-          <div className=" py-16 px-14 flex flex-col items-center justify-start border border-gray-300">
+          <div className="pt-24 pb-20 px-14 flex flex-col items-center justify-start border border-gray-300">
             <div className="flex justify-between gap-10">
               <p>{t("Trinite.trad4")}</p>
               <p>940€</p>
             </div>
             <img src={images[2]} alt="" />
           </div>
-          <div className="py-16 px-14 flex flex-col items-center justify-start border border-gray-300">
+          <div className="pt-20 pb-11 px-14 flex flex-col items-center justify-start border border-gray-300">
             <div className="flex justify-between gap-10">
               <p>{t("Trinite.trad5")}</p>
               <p>4200€</p>
             </div>
             <img src={images[0]} alt="" />
           </div>
-          <div className=" py-16 px-14 flex flex-col items-center justify-start border border-gray-300">
+          <div className="pt-24 pb-20 px-14 flex flex-col items-center justify-start border border-gray-300">
             <div className="flex justify-between gap-10">
               <p>{t("Trinite.trad3")}</p>
               <p>660€</p>
             </div>
             <img className="justify-self-center" src={images[1]} alt="" />
           </div>
-          <div className=" py-16 px-14 flex flex-col items-center justify-start border border-gray-300">
+          <div className="pt-24 pb-20 px-14 flex flex-col items-center justify-start border border-gray-300">
             <div className="flex justify-between gap-10">
               <p>{t("Trinite.trad4")}</p>
               <p>940€</p>
             </div>
             <img src={images[2]} alt="" />
           </div>
-          <div className="py-16 px-14 flex flex-col items-center justify-start border border-gray-300">
+          <div className="pt-20 pb-11 px-14 flex flex-col items-center justify-start border border-gray-300">
             <div className="flex justify-between gap-10">
               <p>{t("Trinite.trad5")}</p>
               <p>4200€</p>
